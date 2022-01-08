a7bfae1e0c745badf23eacf347a4827bd7f59cdb
Jamie Booth <JamieB226@aol.com>
2022-01-07 21:23:42 -0500

    Add files via upload

---
+++ PSWordle.ps1
@@ -0,0 +1,161 @@
+#PSWordle - Wordle Clone in PowerShell
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $WordFile=""
+)
+
+if($wordfile -eq "")
+{
+    $allwords=Invoke-webrequest -Uri "https://github.com/dwyl/english-words/blob/master/words.txt?raw=true"
+    $wordlewords=$allwords.content -split "`n" | where-object {($_ -match "^[a-zA-Z]{6}$") -eq $true}
+}
+else
+{
+    if(Test-Path -Path $WordFile)
+    {
+        $wordlewords=get-content $WordFile | where-object {($_ -match "^[a-zA-Z]{6}$") -eq $true}
+    }
+    else
+    {
+        Write-Output "Word File not found"    
+        $end=$true
+    }
+}
+
+function ScoreWord
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $word,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $guess,
+        [Parameter()]
+        [switch]
+        $scoreOutput
+    )
+
+    if($guess -eq $word)
+    {
+        if(!$scoreOutput)
+        {
+            Write-Host -BackgroundColor "Green" -ForegroundColor "Black" -Object $guess
+            $true
+        }
+        else
+        {
+            0..5 | foreach-object {
+                $bgcolor="black"
+                $fgcolor="white"
+                if($guess[$_] -in $word.ToCharArray())
+                {
+                    $bgcolor="yellow"
+                    $fgcolor="black"
+                }
+                if($guess[$_] -eq $word[$_])
+                {
+                    $bgcolor="green"
+                    $fgcolor="black"
+                }    
+                if($_ -lt 5)
+                {
+                    Write-Host -BackgroundColor $bgcolor -ForegroundColor "black" -Object "_" -NoNewline
+                    Write-Host -BackgroundColor "black" -ForegroundColor "black" -Object "_" -NoNewline
+                }
+                else
+                {
+                    Write-Host -BackgroundColor $bgcolor -ForegroundColor "black" -Object "_"
+                }
+            }
+        }
+    }
+    else
+    {
+        0..5 | foreach-object {
+            $bgcolor="black"
+            $fgcolor="white"
+            if($guess[$_] -in $word.ToCharArray())
+            {
+                $bgcolor="yellow"
+                $fgcolor="black"
+            }
+            if($guess[$_] -eq $word[$_])
+            {
+                $bgcolor="green"
+                $fgcolor="black"
+            }
+            if(!$scoreOutput)
+            {
+                if($_ -lt 5)
+                {
+                    Write-Host -BackgroundColor $bgcolor -ForegroundColor $fgcolor -Object $guess[$_] -NoNewline
+                }
+                else
+                {
+                    Write-Host -BackgroundColor $bgcolor -ForegroundColor $fgcolor -Object $guess[$_]
+                }
+            }
+            else 
+            {
+                if($_ -lt 5)
+                {
+                    Write-Host -BackgroundColor $bgcolor -ForegroundColor "black" -Object "_" -NoNewline
+                    Write-Host -BackgroundColor "black" -ForegroundColor "black" -Object "_" -NoNewline
+                }
+                else
+                {
+                    Write-Host -BackgroundColor $bgcolor -ForegroundColor "black" -Object "_"
+                }
+            }
+        }
+        $false
+    }
+}
+
+while ($end -ne $true)
+{
+    $word=$wordlewords | get-random
+    $guesscount=0
+    $guesses=@()
+    $result=$false
+
+    do
+    {
+        Clear-Host
+        Write-Output $word
+        Write-Host "Enter your guesses one per line.  Guesses will be scored when you press Enter."
+        $guesses | foreach-object{$result=ScoreWord -Word $word -guess $_}
+        if(($guesscount -lt 6) -and ($result -ne $true))
+        {
+            $guess=(Read-host).tolower()
+            if($guess -match "^[a-zA-Z]{6}$")
+            {
+                $guesses+=$guess
+                $guesscount++
+            }
+        }
+        else
+        {
+            $guesscount++    
+        }
+    } while (($guesscount -le 6) -and ($result -ne $true))
+
+    if($result -ne $true)
+    {
+        Write-Host "Word was $word."
+    }
+    else
+    {
+        Write-Host "`nPSWordle $guesscount/6`n"
+        $guesses | foreach-object{$result=ScoreWord -Word $word -guess $_ -ScoreOutput}
+    }
+
+    if((Read-Host -Prompt "`nEnter 'Y' to quit") -eq "Y")
+    {
+        $end=$true
+    }
+}
